7e4e254d10561cc0c7cc423df51bc1e57a72c6a3
Doge_KingYT1 <dogekingyt1@gmail.com>
2024-03-18 00:19:00 -0400

    3/18/24

--- scripts/test.lua
+++ scripts/test.lua
@@ -2,3 +2,12 @@ local msg = "Fuck ROBLOX & Byfron Technologies"
 
 print(msg)
 printidentity()
+
+game.StarterGui:SetCore(
+		"SendNotification",
+		{
+			Title = "Loadstring Tester",
+			Duration = 3,
+			Text = "Loadstring Works 👍"
+		}
+	)
