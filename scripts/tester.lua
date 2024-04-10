895f7d15a52718d8a94c95a18a3cab6d6ecb6f3d
Doge_KingYT1 <dogekingyt1@gmail.com>
2024-04-10 12:24:45 -0400

    4/10/24

--- scripts/tester.lua
+++ scripts/tester.lua
@@ -1,6 +1,11 @@
 local msg = "Fuck ROBLOX & Byfron Technologies"
+local msg2 = "Loadstring Works. :) 👍"
 
+
+print("Username" ..game.Players.LocalPlayer.Name)
+print("Display Name:" ..game.Players.LocalPlayer.DisplayName)
 print(msg)
+print(msg2)
 printidentity()
 
 game.StarterGui:SetCore(
